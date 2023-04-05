55fc402601330b9d4f2e5e1e06fdb82e67c58c6f
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-04-04 21:28:54 -0300

    🌱 update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -16,13 +16,44 @@ async function main() {
       title: 'Example pool',
       code: 'BOLL1234',
       ownerId: user.id,
+
+      participant: {
+        create: {
+          userId: user.id,
+        },
+      },
     }
   });
 
-  const participant = await prisma.participant.create({
+  await prisma.game.create({
     data: {
-      poolId: pool.id,
-      userId: user.id,
+      date: '2023-04-04T12:00:00.201Z',
+      firstTeamCountryCode: 'DE',
+      secondTeamCountryCode: 'BR',
+    }
+  });
+
+  await prisma.game.create({
+    data: {
+      date: '2023-04-04T12:00:00.201Z',
+      firstTeamCountryCode: 'BR',
+      secondTeamCountryCode: 'AR',
+
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoints: 1,
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id,
+              },
+            },
+          },
+        },
+      },
     }
   });
 }
